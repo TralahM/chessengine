4b8c2b45dce2f06794ca6b24bd6bf1d02ab52aee
TralahM <musyoki.tralah@students.jkuat.ac.ke>
2020-03-21 00:57:40 +0300

    negating unary ops

--- engine/bitboard.py
+++ engine/bitboard.py
@@ -1,16 +1,35 @@
 class Bitboard:
     def __init__(self, bitstr=f'{0:064b}', * args, **kwargs):
+        if isinstance(bitstr, int):
+            self.bval = f'{bitstr:064b}'
         self.bval = bitstr
 
     @property
     def as_int(self):
         return int(self.bval, 2)
 
+    @property
+    def lsb(self):
+        return self & -self
+
+    @property
+    def with_reset_lsb(self):
+        return self & self.__class__(f'{self.as_int-1:064b}')
+
     def __str__(self):
         return self.bval
 
     def __repr__(self):
         return self.bval
+
+    def __add__(self, other):
+        return self.__class__(f'{self.as_int+other.as_int:064b}')
+
+    def __neg__(self):
+        return self.__class__(f'{self.as_int*-1:064b}')
+
+    def __sub__(self, other):
+        return self.__class__(f'{self.as_int-other.as_int:064b}')
 
     def __and__(self, other):
         return self.__class__(f'{self.as_int&other.as_int:064b}')
